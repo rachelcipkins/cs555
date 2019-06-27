c97a6c6b234a4129a0ed6aad33f6621970b617ed
rcipkins <rcipkins@stevens.edu>
2019-06-27 13:20:29 -0400

    Moved test file.

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+import os
+from GEDCOM_parser import checkBirthBeforeMarriageOfParents, lessThan150YearsOld, parse, getFamInfo
+
+path = os.path.dirname(__file__)
+testFile = os.path.relpath("testGEDCOM.ged", path)
+
+#class US07Tests(unittest.TestCase):
+
+class US08Tests(unittest.TestCase):
+    def testBirthBeforeMarriage(self):
+        valid = parse(testFile)
+        individuals, families = getFamInfo(valid)
+        self.assertTrue(checkBirthBeforeMarriageOfParents(individuals, families))
+
+
+if __name__ == "__main__":
+    unittest.main() # run all tests
