df74f61894e57629d8de243547faa243f00ced6e
rcipkins <rcipkins@stevens.edu>
2019-06-27 12:47:04 -0400

    Started to add test cases for US07 and US08.

--- testing/test.py
+++ testing/test.py
@@ -1,8 +1,15 @@
 import unittest
+from GEDCOM_parser import checkBirthBeforeMarriageOfParents, lessThan150YearsOld
+
+path = os.path.dirname(__file__)
+testFile = os.path.relpath("..\\testGEDCOM.ged", path)
 
 class US07Tests(unittest.TestCase):
 
 class US08Tests(unittest.TestCase):
+    def testBirthBeforeMarriage(self):
+        individuals, families = GEDCOMParser(testFile)
+        self.assertTrue(birth_before_marriage(individuals, families))
 
 
 if __name__ == "__main__":
